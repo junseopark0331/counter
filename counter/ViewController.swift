05916f338f970f8daee6603613eb8482a37ca5a7
junseopark0331 <junseopark0331@icloud.com>
2022-07-03 17:55:49 +0900

    1. 화면은 [현재 수 텍스트, +버튼, -버튼, Reset버튼] 으로 구성 2. View가 Load되거나 Reset버튼을 누르면 비밀 수 하나가 생성된다. 3. 현재 수 텍스트는 비밀 수보다 크면 파란색으로 / 비밀 수와 같으면 초록색으로 / 비밀 수보다 작으면 빨간색으로 textColor가 바뀐다. 4. +버튼을 누르면 현재 수가 +1 된다, -버튼을 누르면 현재 수가 -1 된다.
    이 가능한 게임을 만들어봤다.

--- counter/ViewController.swift
+++ counter/ViewController.swift
@@ -9,11 +9,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var plusButton: UIButton!
+    @IBOutlet weak var minusButton: UIButton!
+    @IBOutlet weak var resetButton: UIButton!
+    @IBOutlet weak var label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    var number: Int = 0
+    var random = arc4random_uniform(15) + 1
+    @IBAction func plusButton(_ sender: Any) {
+        number += 1
+        if number < random {
+            label.textAlignment = .center
+            label.textColor = UIColor.red
+            self.label.text = "\(number)"
+        }else if number == random{
+            label.textAlignment = .center
+            label.textColor = UIColor.green
+            self.label.text = "\(number)"
+        }else{
+            label.textAlignment = .center
+            label.textColor = UIColor.blue
+            self.label.text = "\(number)"
+        }
+    }
+    @IBAction func minusButton(_ sender: Any) {
+        number -= 1
+        if number < random {
+            label.textAlignment = .center
+            label.textColor = UIColor.red
+            self.label.text = "\(number)"
+        }else if number == random{
+            label.textAlignment = .center
+            label.textColor = UIColor.green
+            self.label.text = "\(number)"
+        }else{
+            label.textAlignment = .center
+            label.textColor = UIColor.blue
+            self.label.text = "\(number)"
+        }
+    }
+    @IBAction func resetButton(_ sender: Any){
+        random = (arc4random_uniform(15) + 1)
+    }
 }
 
